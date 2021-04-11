38468b0708d4721f91d815a6c770f7006cfa17f0
Sergio Orejarena <sergioorueda@gmail.com>
2021-04-11 23:29:46 +0000

    Runs Shell

---
+++ main.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * main - Runs the shell.
+ *
+ * Return: Always (0).
+ */
+
+int main(void)
+{
+	char *line = NULL;
+	char **tokens = NULL;
+	int stats = 0;
+
+	while (stats != EOF)
+	{
+		signal(SIGINT, _signal);
+
+		if (isatty(STDIN))
+			write(STDOUT, "$ ", 2);
+
+		line = read_line();
+		tokens = tokenize(line);
+		stats = execute(tokens);
+
+		free(line);
+		free(tokens);
+	}
+
+	return (0);
+}
