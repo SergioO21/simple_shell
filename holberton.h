91c20f0a5e999fd759a1c76e70d6b6441d198a50
jblanco75 <jblanco75@gmail.com>
2021-04-14 01:11:21 +0000

    Library

--- holberton.h
+++ holberton.h
@@ -21,13 +21,14 @@ extern char **environ;
 /** Prototypes */
 char **tokenize(char *buffer);
 char *check_path(char **ar);
+void _error(char *command);
 int builtins(char **ar);
 int execute(char **ar);
 void _fork(char **ar);
 int launch(char **ar);
 char *read_line(void);
-char *get_env(void);
 void _signal(int sig);
+char *get_env(void);
 
 /** Clone Functions */
 int _strncmp(char *s1, char *s2, int len);
