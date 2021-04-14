c48925975d1e594ab4b4e1eaab4abe448ff5516b
Sergio Orejarena <sergioorueda@gmail.com>
2021-04-14 01:32:29 +0000

    betty error

--- _error.c
+++ _error.c
@@ -1,11 +1,9 @@
 #include "holberton.h"
 
 /**
+ * _error - Print the standard error.
  *
- *
- *
- *
- *
+ * @command: Entered command.
  */
 
 void _error(char *command)
@@ -13,7 +11,7 @@ void _error(char *command)
 	int len = 0;
 
 	len = _strlen(command);
-	
+
 	write(STDOUT, command, len);
 	write(STDOUT, ": No such file or directory\n", 28);
 }
